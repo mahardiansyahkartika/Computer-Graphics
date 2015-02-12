cad68abc0d6adab79bcd2868dd8f9fbbdf6bc8e8
Mahardiansyah Kartika <mahardiansyahkartika@gmail.com>
2015-02-12 14:42:25 -0500

    + add reflection shader

--- p2 - Geometry/shaders/material_frag.glsl
+++ p2 - Geometry/shaders/material_frag.glsl
@@ -3,11 +3,13 @@ varying vec3 cam_dir;
 varying vec3 color;
 
 // Declare any additional variables here. You may need some uniform variables.
+uniform samplerCube cubeMap;
 
 void main(void)
 {
 	// Your shader code here.
 	// Note that this shader won't compile since gl_FragColor is never set.
-
-	// gl_FragColor = [color]
+	
+	vec3 reflectedDirection = normalize(reflect(normalize(cam_dir), normalize(norm)));
+	gl_FragColor = textureCube(cubeMap, reflectedDirection);
 }
